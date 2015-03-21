b63a9615fa428c3f70cb81f9b61c6c0f2a9187bd
stefan-11 <stefan-11@gmx.de>
2015-03-22 00:12:22 +0100

    INTERNAL minor change

--- src/io/stefanwill/Main.java
+++ src/io/stefanwill/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public void sayHello(){
         System.out.println("Hello World.");
         System.out.println("Hello again.");
+        System.out.println("Huhu");
     }
 
 }
