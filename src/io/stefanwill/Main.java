3ac3ddaf886edb97cbd842eb8abc6cd51be64a74
Stefan Will <stefan-github@gmx.de>
2015-03-22 00:06:50 +0100

    Start of project

---
+++ src/io/stefanwill/Main.java
@@ -0,0 +1,17 @@
+package io.stefanwill;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        Main myMain = new Main();
+
+        myMain.sayHello();
+    }
+
+    public void sayHello(){
+        System.out.println("Hello World.");
+        System.out.println("Hello again.");
+    }
+
+}
